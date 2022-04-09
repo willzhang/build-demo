704d1ae4861b01c0d51fb192038b8c6d6a4ded42
will <willzhmic@outlook.com>
2022-04-09 12:41:06 +0800

    Update greeting.js

--- node-hello-world/routes/greeting.js
+++ node-hello-world/routes/greeting.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET greeting */
-router.get('/', function(req, res, next) {
-  res.send(
-    { "greeting": "Hello, world!" }
-  );
-});
+// middleware that is specific to this router
+router.use((req, res, next) => {
+          console.log('Time: ', Date.now())
+          next()
+})
+
+// define the home page route
+router.get('/', (req, res) => {
+          res.send('greeting home page')
+})
+
+// define the about route
+router.get('/about', (req, res) => {
+          res.send('About greeting')
+})
 
 module.exports = router;
